f1cac95e175124560a423702d7f0cc254ed7c670
Artem Romaniuk <artem.romaniuk@gmail.com>
2018-09-27 00:20:27 +0300

    Make directories structure

---
+++ app/Core/Controller/Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Core\Controller;
+
+
+abstract class Controller
+{
+    abstract public function index();
+
+    abstract public function taxonomy();
+}
--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
 
+global $wp_query;
+
+$queriedObject = $wp_query->get_queried_object();
+
+if ($queriedObject instanceof \WP_Post)
+{
+    $type = $queriedObject->post_type;
+
+    var_dump($type);
+}
+
+if ($queriedObject instanceof \WP_Term)
+{
+    $taxonomy = $queriedObject->taxonomy;
+
+    var_dump($taxonomy);
+}
+
+if ($queriedObject instanceof \WP_User)
+{
+
+}
