b817d176351a93e7eccab311cec3a50a284610cc
Artem Romaniuk <artem.romaniuk@gmail.com>
2018-09-26 22:49:03 +0300

    Init project

--- functions.php
+++ functions.php
@@ -1,1 +1,12 @@
 <?php
+
+show_admin_bar(false);
+
+define('TEXT_DOMAIN', 'default');
+
+define('TEMPLATE_DIR', get_template_directory());
+
+define('TEMPLATE_URL', get_template_directory_uri());
+
+
+require_once TEMPLATE_DIR . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
