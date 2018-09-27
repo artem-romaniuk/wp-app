05e52687e9ec3cadfaee58b6f436836e9e157677
Artem Romaniuk <artem.romaniuk@gmail.com>
2018-09-27 22:31:13 +0300

    FileSystem class

--- app/Core/FileSystem/FileSystem.php
+++ app/Core/FileSystem/FileSystem.php
@@ -17,7 +17,7 @@ class FileSystem
             return file_get_contents($path);
         }
 
-        throw new FileNotFoundException("File does not exist at path {$path}");
+        throw new \Exception("File does not exist at path {$path}");
     }
 
 
@@ -184,62 +184,19 @@ class FileSystem
         return glob($pattern, $flags);
     }
 
-    /**
-     * Get an array of all files in a directory.
-     *
-     * @param  string  $directory
-     * @param  bool  $hidden
-     * @return \Symfony\Component\Finder\SplFileInfo[]
-     */
-    public function files($directory, $hidden = false)
+
+    public function files($directory)
     {
-        return iterator_to_array(
-            Finder::create()->files()->ignoreDotFiles(! $hidden)->in($directory)->depth(0)->sortByName(),
-            false
-        );
+        //
     }
 
-    /**
-     * Get all of the files from the given directory (recursive).
-     *
-     * @param  string  $directory
-     * @param  bool  $hidden
-     * @return \Symfony\Component\Finder\SplFileInfo[]
-     */
-    public function allFiles($directory, $hidden = false)
-    {
-        return iterator_to_array(
-            Finder::create()->files()->ignoreDotFiles(! $hidden)->in($directory)->sortByName(),
-            false
-        );
-    }
 
-    /**
-     * Get all of the directories within a given directory.
-     *
-     * @param  string  $directory
-     * @return array
-     */
     public function directories($directory)
     {
-        $directories = [];
-
-        foreach (Finder::create()->in($directory)->directories()->depth(0)->sortByName() as $dir) {
-            $directories[] = $dir->getPathname();
-        }
-
-        return $directories;
+        //
     }
 
-    /**
-     * Create a directory.
-     *
-     * @param  string  $path
-     * @param  int     $mode
-     * @param  bool    $recursive
-     * @param  bool    $force
-     * @return bool
-     */
+
     public function makeDirectory($path, $mode = 0755, $recursive = false, $force = false)
     {
         if ($force) {
@@ -247,153 +204,5 @@ class FileSystem
         }
 
         return mkdir($path, $mode, $recursive);
-    }
-
-    /**
-     * Move a directory.
-     *
-     * @param  string  $from
-     * @param  string  $to
-     * @param  bool  $overwrite
-     * @return bool
-     */
-    public function moveDirectory($from, $to, $overwrite = false)
-    {
-        if ($overwrite && $this->isDirectory($to)) {
-            if (! $this->deleteDirectory($to)) {
-                return false;
-            }
-        }
-
-        return @rename($from, $to) === true;
-    }
-
-    /**
-     * Copy a directory from one location to another.
-     *
-     * @param  string  $directory
-     * @param  string  $destination
-     * @param  int     $options
-     * @return bool
-     */
-    public function copyDirectory($directory, $destination, $options = null)
-    {
-        if (! $this->isDirectory($directory)) {
-            return false;
-        }
-
-        $options = $options ?: FilesystemIterator::SKIP_DOTS;
-
-        // If the destination directory does not actually exist, we will go ahead and
-        // create it recursively, which just gets the destination prepared to copy
-        // the files over. Once we make the directory we'll proceed the copying.
-        if (! $this->isDirectory($destination)) {
-            $this->makeDirectory($destination, 0777, true);
-        }
-
-        $items = new FilesystemIterator($directory, $options);
-
-        foreach ($items as $item) {
-            // As we spin through items, we will check to see if the current file is actually
-            // a directory or a file. When it is actually a directory we will need to call
-            // back into this function recursively to keep copying these nested folders.
-            $target = $destination.'/'.$item->getBasename();
-
-            if ($item->isDir()) {
-                $path = $item->getPathname();
-
-                if (! $this->copyDirectory($path, $target, $options)) {
-                    return false;
-                }
-            }
-
-            // If the current items is just a regular file, we will just copy this to the new
-            // location and keep looping. If for some reason the copy fails we'll bail out
-            // and return false, so the developer is aware that the copy process failed.
-            else {
-                if (! $this->copy($item->getPathname(), $target)) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * Recursively delete a directory.
-     *
-     * The directory itself may be optionally preserved.
-     *
-     * @param  string  $directory
-     * @param  bool    $preserve
-     * @return bool
-     */
-    public function deleteDirectory($directory, $preserve = false)
-    {
-        if (! $this->isDirectory($directory)) {
-            return false;
-        }
-
-        $items = new FilesystemIterator($directory);
-
-        foreach ($items as $item) {
-            // If the item is a directory, we can just recurse into the function and
-            // delete that sub-directory otherwise we'll just delete the file and
-            // keep iterating through each file until the directory is cleaned.
-            if ($item->isDir() && ! $item->isLink()) {
-                $this->deleteDirectory($item->getPathname());
-            }
-
-            // If the item is just a file, we can go ahead and delete it since we're
-            // just looping through and waxing all of the files in this directory
-            // and calling directories recursively, so we delete the real path.
-            else {
-                $this->delete($item->getPathname());
-            }
-        }
-
-        if (! $preserve) {
-            @rmdir($directory);
-        }
-
-        return true;
-    }
-
-    /**
-     * Remove all of the directories within a given directory.
-     *
-     * @param  string  $directory
-     * @return bool
-     */
-    public function deleteDirectories($directory)
-    {
-        $allDirectories = $this->directories($directory);
-
-        if (! empty($allDirectories)) {
-            foreach ($allDirectories as $directoryName) {
-                $this->deleteDirectory($directoryName);
-            }
-
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * Empty the specified directory of all files and folders.
-     *
-     * @param  string  $directory
-     * @return bool
-     */
-    public function cleanDirectory($directory)
-    {
-        return $this->deleteDirectory($directory, true);
-    }
-
-    private function normalizeDir($dir)
-    {
-        return rtrim($dir, '/' . \DIRECTORY_SEPARATOR);
     }
 }
