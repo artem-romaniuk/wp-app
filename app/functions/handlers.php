16fd462fb8a0ab84c1316536a5bc221cd614fc4b
Artem Romaniuk <artem.romaniuk@gmail.com>
2018-10-17 22:29:57 +0300

    other

--- app/functions/handlers.php
+++ app/functions/handlers.php
@@ -11,16 +11,16 @@ function callbackHandler() {
 }
 
 /* Form in frontend */
-//?>
-<!--<form action="--><?php //echo appConfig('post_handler', '/'); ?><!--">-->
-<!--    --><?php //wp_nonce_field('callback_action','callback_field'); ?>
-<!--    <input type="hidden" name="action" value="callback_form">-->
-<!---->
-<!--    <input type="text" placeholder="--><?php //echo trans('Ваше имя'); ?><!--" name="name" class="inpt name">-->
-<!--    <input type="text" placeholder="--><?php //echo trans('Ваш телефон'); ?><!--" name="phone" class="inpt phone">-->
-<!--    <button type="submit" class="inpt sub fill-right">--><?php //echo trans('Отправить заявку'); ?><!--</button>-->
-<!--</form>-->
-<?php
+
+//<form action="<php echo appConfig('post_handler', '/'); >">
+//    <?php wp_nonce_field('callback_action','callback_field'); >
+//    <input type="hidden" name="action" value="callback_form">
+//
+/*    <input type="text" placeholder="<php echo trans('Ваше имя'); ?>" name="name" class="inpt name">*/
+/*    <input type="text" placeholder="<php echo trans('Ваш телефон'); ?>" name="phone" class="inpt phone">*/
+//    <button type="submit" class="inpt sub fill-right"><php echo trans('Отправить заявку'); ></button>
+//</form>
+
 /* End Form in frontend */
 
 /* Callback form Handler */
